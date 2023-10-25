d4f50da2a3bbd0354c1cc6eda86c3cc74068623c
Danilo Grieco <grieco.danilo@gmail.com>
2023-10-25 09:45:36 +0200

    Full compatibility with try

--- lib/sentry/core_ext/object/try.rb
+++ lib/sentry/core_ext/object/try.rb
@@ -1,8 +1,24 @@
-class Object
-  def try(method_name, *args)
-    return nil unless respond_to?(method_name)
+# using the ruby/rails compatibility matrix I have taken the implementation of AS 4.2.11.3
+# 4.2.11.3 (ruby.2.2 reccomended) < 5 (ruby 2.4 reccomended => min sentry official version)
 
-    public_send(method_name, *args)
+class Object
+  def try(*a, &b)
+    try!(*a, &b) if a.empty? || respond_to?(a.first)
+  end
+
+  # Same as #try, but will raise a NoMethodError exception if the receiver is not +nil+ and
+  # does not implement the tried method.
+
+  def try!(*a, &b)
+    if a.empty? && block_given?
+      if b.arity == 0
+        instance_eval(&b)
+      else
+        yield self
+      end
+    else
+      public_send(*a, &b)
+    end
   end
 end
 
@@ -10,4 +26,9 @@ class NilClass
   def try(*args)
     nil
   end
+
+  def try!(*args)
+    nil
+  end
+
 end
